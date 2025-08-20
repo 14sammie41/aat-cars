44d656b7c42148ae7892ff1705a8ea61ac01088f
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-08-20 15:01:17 +0100

    Set up initial project and app and tested to ensure viewable in server

--- aatcars/wsgi.py
+++ aatcars/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cartalk project.
+WSGI config for aatcars project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cartalk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aatcars.settings')
 
 application = get_wsgi_application()
