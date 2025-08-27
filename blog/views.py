c5d09e590ac0bfb1ac108dab8c3e24e15cc2a69f
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-08-27 14:33:02 +0100

    Added pagination connections

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
+    # Chosen to show 6 per page for user readability and flex design
     paginate_by = 6
