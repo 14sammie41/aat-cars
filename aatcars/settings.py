e4d03deae8bc861b9fe7627472ef76e00fb50e10
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-09-06 19:43:32 +0100

    Added the first try of an image option in my Post model

--- aatcars/settings.py
+++ aatcars/settings.py
@@ -156,8 +156,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Media files (settings for user-uploaded content)
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
