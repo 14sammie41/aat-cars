88cdd49acedab21019154c08a76920cf968d2fa9
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-09-19 20:18:11 +0100

    Added the full POST in contactus page and started linking it up

--- contactus/models.py
+++ contactus/models.py
@@ -8,3 +8,12 @@ class Contactus(models.Model):
 
     def __str__(self):
         return self.title
+    
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.name} <{self.email}>"
--- contactus/views.py
+++ contactus/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Contactus
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import ContactMessage, Contactus
 
 # Create your views here.
 def contact_us(request):
@@ -8,6 +9,13 @@ def contact_us(request):
     """
     contactus = Contactus.objects.all().order_by('-updated_on').first()
 
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("enquiry")
+        ContactMessage.objects.create(name=name, email=email, message=message)
+        messages.success(request, "Thank you for your message, we will get back to you as soon as possible.")
+        return redirect("contactus")
     return render(
         request,
         "contactus/contactus.html",
