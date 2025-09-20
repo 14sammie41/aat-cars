917cc180229615d674a7343c848d85b881992199
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-09-20 18:27:35 +0100

    Added the editing comment functionality

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import CommentForm
 
 # Create your views here.
@@ -49,3 +50,30 @@ def post_detail(request, slug):
             "comment_form": comment_form,
         },
     )
+    
+def comment_edit(request, slug, comment_id):
+    """
+    View to edit comments
+    """
+    if request.method == "POST":
+        
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, id=comment_id, post=post)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+        
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment updated'
+            )
+        else:
+            messages.add_message(
+                request, messages.ERROR,
+                'Error updating comment'
+            )
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
